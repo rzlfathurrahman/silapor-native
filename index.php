b22cec3452d547ac36537cb14365e335570cb320
zalfaone <githubakun686@gmail.com>
2022-02-10 15:23:04 +0700

    change login style with bootstrap

--- index.php
+++ index.php
@@ -5,39 +5,39 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>SILAPOR - Login</title>
+
+  <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+
+  <!-- Your Custom CSS -->
+  <link rel="stylesheet" href="./assets/css/login.css">
+
 </head>
-<body>
-  <!-- <div style="display: block; position:absolute; z-index:9999; top:0;bottom:0;left:0;right:0;background-color:black;"></div> -->
-  <div class="container">
-    <header>
-      <h1 class="judul">SILAPOR</h1>
-      <h3 class="deskripsi">(SISTEM INFORMASI LAPORAN PENGADUAN MASYARAKAT)</h3>
-    </header>
-      <div class="badan">
-        <form action="masy_proses_login.php" method="post">
-          <table>
-            <tr>
-              <td>Username</td>
-              <td>:</td>
-              <td><input type="text" name="username" placeholder="username"></td>
-            </tr>
-            <tr>
-              <td>Password</td>
-              <td>:</td>
-              <td><input type="password" name="password" type="password" placeholder="password"></td>
-            </tr>
-            <tr>
-              <td >
-                <button class="btn btn-primary btn-sm" type="submit" name="login">Login</button>
-              </td>
-            </tr>
-          </table>
-          <br>
-          Belum punya akun? <a href="masy_registrasi.php">Daftar disini</a>
-        </form>
+<body class="text-center">
+  <main class="form-signin">
+    <form>
+      <img class="mb-4" src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="Website Logo" width="72" height="57">
+      <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+      <div class="form-floating mb-3">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Email address</label>
       </div>
-  </div>
+      
+      <div class="form-floating mb-3">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Password</label>
+      </div>
+
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+      </div>
+      <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+    </form>
+  </main>
 
   <script src="./assets/js/bootstrap.bundle.min.js"></script>
 </body>
