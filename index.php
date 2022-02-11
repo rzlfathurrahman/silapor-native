6d398897a83a3e0b6e469ac78b50b43f7abda573
zalfaone <githubakun686@gmail.com>
2022-02-11 11:29:11 +0700

    include header and footer layout to login page

--- index.php
+++ index.php
@@ -1,19 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>SILAPOR - Login</title>
+<?php 
+  require_once 'layouts/header.php';
+?>
 
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
-
-  <!-- Your Custom CSS -->
-  <link rel="stylesheet" href="./assets/css/login.css">
-
-</head>
-<body>
   <main class="form-signin">
     <form action="masy_proses_login.php" method="post">
       <!-- <img class="mb-4" src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="Website Logo" width="72" height="57"> -->
@@ -39,12 +27,12 @@
 
       <hr style="width: 50%;" class="mx-auto">
       
-      <p class="text-center">Belum punya akun? <a href="masy_registrasi.php">Daftar disini</a> </p>
+      <p class="text-center">Belum punya akun? <a href="daftar">Daftar disini</a> </p>
 
       <p class="mt-5 mb-3 text-center text-muted">&copy; <?= date('Y') ?></p>
     </form>
   </main>
 
-  <script src="./assets/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+<?php 
+  require_once 'layouts/footer.php';
+?>
