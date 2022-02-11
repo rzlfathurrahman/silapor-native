80ad69673b697cdf8044bfa31275c6b0b2007c1d
zalfaone <githubakun686@gmail.com>
2022-02-11 11:28:36 +0700

    create html template layout with bootstrap css

---
+++ layouts/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SILAPOR - Login</title>
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+
+  <!-- Your Custom CSS -->
+  <link rel="stylesheet" href="./assets/css/login.css">
+
+</head>
+<body>
