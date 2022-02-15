db15b7225147da21e0b0924d4b0d2d58656dca6f
zalfaone <githubakun686@gmail.com>
2022-02-15 14:25:23 +0700

    create method for parsing url into array

--- app/core/App.php
+++ app/core/App.php
@@ -2,11 +2,31 @@
 
 class App  
 { 
+
   public function __construct()
   {
     // Logic
-    echo "Web OK";
+    $url = $this->parseURL();
+    var_dump($url);
   }
+
+  public function parseURL(){
+    if(isset($_GET['url'])){
+
+      // remove '/' at the end of url string
+      $url = rtrim($_GET['url'],'/');
+
+      // filter unwanted chars for security
+      $url = filter_var($url,FILTER_SANITIZE_URL);
+
+      // splice the string into array (controller,method, and data)
+      $url = explode('/',$url);
+
+      // return the parsed url
+      return $url;
+    }
+  }
+
 }
   
 
